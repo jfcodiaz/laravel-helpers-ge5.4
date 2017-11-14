02d6422dd59734577e634b3249e81830958987ad
José Francisco Díaz <jfcodiaz@gmail.com>
2017-11-14 11:02:21 -0600

    Se agrega el método isMine al modelo y constantes para su uso

--- src/Model/ModelBase.php
+++ src/Model/ModelBase.php
@@ -2,7 +2,14 @@
 namespace DevTics\LaravelHelpers\Model;
 
 use Illuminate\Database\Eloquent\Model;
+
+
+
 class ModelBase extends Model {
+    
+    const ACTION_CHECK_ABORT_404 = 0;
+    const ACTION_CHECK_THOW_EXCEPTION = 1;
+    const ACTION_CHECK_RETURN_BOOLEAN = 2;
     
     use \DevTics\LaravelHelpers\Model\traits\MethodsModelBase;
     protected $guarded = ['id'];    
