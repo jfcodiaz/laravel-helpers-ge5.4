db1d8f7886d4659650ac1faff3f7c75b4a5e9263
José Francisco Díaz <jfcodiaz@gmail.com>
2018-04-26 17:08:27 -0500

    Se retira la dependencia con graham-campbell/htmlmin
    Queda pendiente encontrar un minificador con soporte a Laravel 5.6

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,19 +5,22 @@ use Illuminate\Foundation\AliasLoader;
 
 class ServiceProvider extends SP {
     public function boot() {
-        
+
     }
-    
+
     public function register() {
-        /* maatwebsite/exce 
+        /* maatwebsite/exce
         /* http://www.maatwebsite.nl/laravel-excel/docs */
         $this->app->register(\Maatwebsite\Excel\ExcelServiceProvider::class);
         AliasLoader::getInstance()->alias('Excel', \Maatwebsite\Excel\Facades\Excel::class);
-        
+
         /* HTMLMin */
-        $this->app->register(\GrahamCampbell\HTMLMin\HTMLMinServiceProvider::class);
-        AliasLoader::getInstance()->alias("HTMLMin", \GrahamCampbell\HTMLMin\Facades\HTMLMin::class);
-        
+        /**
+        * @TODO: Agregrar soporte a un nuevo minificador para laravel 5.6  
+        */
+//        $this->app->register(\GrahamCampbell\HTMLMin\HTMLMinServiceProvider::class);
+//        AliasLoader::getInstance()->alias("HTMLMin", \GrahamCampbell\HTMLMin\Facades\HTMLMin::class);
+
         /* Laroute */
         $this->app->register(\Lord\Laroute\LarouteServiceProvider::class);
     }
